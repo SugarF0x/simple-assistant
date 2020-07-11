cc3e580443efb2d3d36c604b7775acbfbc8bfca2
Sugar_F0x <malion.torchwood@yandex.ru>
2020-07-11 12:33:46 +0300

    :white_check_mark: Add profit display
    > Display amount of gold won/lost over the script runtime

--- assets/content.js
+++ assets/content.js
@@ -8,53 +8,74 @@ let form = $('#submit');
 let states = {
   isActive: null,
   current:  null,
-  baseline: null
+  baseline: null,
+  profit: null
 };
 
-chrome.storage.sync.get("isActive", state => {
-  states.isActive = state.isActive === 'true' ? 1 : 0;
-  chrome.storage.sync.get("baseline", bet => {
-    states.baseline = bet.baseline || 1;
-    chrome.storage.sync.get("current", bet => {
-      if (isFailure) {
-        states.current = bet.current*2;
-      } else if (isSuccess) {
-        states.current = states.baseline;
-      } else {
-        states.current = bet.current || states.baseline;
-      }
-      chrome.storage.sync.set({current: states.current});
+chrome.storage.sync.get("profit", state => {
+  states.profit = state.profit || 0;
+  chrome.storage.sync.get("isActive", state => {
+    states.isActive = state.isActive === 'true' ? 1 : 0;
+    chrome.storage.sync.get("baseline", bet => {
+      states.baseline = bet.baseline || 1;
+      chrome.storage.sync.get("current", bet => {
+        states.current = parseInt(bet.current) || parseInt(states.baseline);
+        if (isFailure) {
+          states.profit -= states.current;
+          states.current = bet.current*2;
+        } else if (isSuccess) {
+          states.profit += states.current;
+          states.current = states.baseline;
+        } else {
+          states.profit = 0;
+        }
+        chrome.storage.sync.set({current: states.current});
+        chrome.storage.sync.set({profit: states.profit});
 
-      function timeout() {
-        setTimeout(() => {
-          input.val(states.current);
-
+        function timeout() {
           setTimeout(() => {
-            form.submit();
-          },250)
-          }, Math.floor(Math.random()*1000)+250)
-      }
+            input.val(states.current);
 
-      let header = $('.kt-subheader__title');
-      let newHeader = ` | Gambler: [ ${states.isActive} | ${states.baseline} | ${states.current} ]`;
-      header.append(newHeader);
+            setTimeout(() => {
+              form.submit();
+            },250)
+          }, Math.floor(Math.random()*1000)+1250)
+        }
 
-      let initButton = $('<button></button>')
-        .text(states.isActive ? 'Stop' : 'Begin')
-        .css({"margin-left": '1rem', "border-radius": "1rem"})
-        .click(() => {
-          if (states.isActive) {
-            chrome.storage.sync.set({isActive: 'false'})
-          } else {
-            chrome.storage.sync.set({isActive: 'true'});
-            timeout()
-          }
-        });
-      header.append(initButton);
+        let header = $('.kt-subheader__title');
+        let newHeader = ` | Gambler: [ ${states.isActive} | ${states.baseline} | ${states.current} ]`;
+        header.append(newHeader);
 
-      if (states.isActive) {
-        timeout();
-      }
+        let profitDisplay = $('<span></span>');
+        if (states.profit > 0) {
+          profitDisplay.text(`+${states.profit}`);
+          profitDisplay.css({"color": "green"})
+        } else if (states.profit < 0) {
+          profitDisplay.text(`${states.profit}`);
+          profitDisplay.css({"color": "red"})
+        } else {
+          profitDisplay.text(states.profit);
+        }
+        profitDisplay.css({"margin-left": '1rem'});
+        header.append(profitDisplay);
+
+          let initButton = $('<button></button>')
+          .text(states.isActive ? 'Stop' : 'Begin')
+          .css({"margin-left": '1rem', "border-radius": "1rem"})
+          .click(() => {
+            if (states.isActive) {
+              chrome.storage.sync.set({isActive: 'false'})
+            } else {
+              chrome.storage.sync.set({isActive: 'true'});
+              timeout()
+            }
+          });
+        header.append(initButton);
+
+        if (states.isActive) {
+          timeout();
+        }
+      });
     });
   });
 });
