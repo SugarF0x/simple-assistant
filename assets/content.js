929a997a2ac6e04fbf8c039d0ffebcadad75d94a
Sugar_F0x <malion.torchwood@yandex.ru>
2020-07-14 16:54:02 +0300

    :recycle: Move tabs functions to dedicated engine object
    > Every tab is its own engine property
      * Every tab has its own data property that can be referenced from
        outside
    > Enable plugin throughout the whole site

--- assets/content.js
+++ assets/content.js
@@ -1,99 +1,129 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
 
-function gamble5050() {
-  let isSuccess = ($('.notice-success').length > 0);
-  let isFailure = ($('.notice-danger').length > 0);
-  let input = $('#sample1');
-  let form = $('#submit');
+const engine = {
+  gamble: {
+    oneInTwo: {
+      data: {
+        isActive: null,
+        current:  null,
+        baseline: null,
+        profit: null
+      },
+      roll:() => {
+        let data = engine.gamble.oneInTwo.data;
+        
+        let isSuccess = ($('.notice-success').length > 0);
+        let isFailure = ($('.notice-danger').length > 0);
+        let input = $('#sample1');
+        let form = $('#submit');
 
-  let states = {
-    isActive: null,
-    current:  null,
-    baseline: null,
-    profit: null
-  };
+        // TODO: add gold/min display
+        // TODO: add lose-streak display and fail-safe for when lose-streak goes too bad
+        // TODO: add longers win/lose streaks this runtime
+        // TODO: add input for baseline value inside DOM
+        // TODO: fix bug with storage overflow after long term use
+        // TODO: add documentation
+        // TODO: use async await? question mark?
 
-// TODO: add gold/min display
-// TODO: add lose-streak display and fail-safe for when lose-streak goes too bad
-// TODO: add longers win/lose streaks this runtime
-// TODO: add input for baseline value inside DOM
-// TODO: fix bug with storage overflow after long term use
-// TODO: add documentation
-// TODO: use async await? question mark?
+        chrome.storage.sync.get("profit", state => {
+          data.profit = state.profit || 0;
+          chrome.storage.sync.get("isActive", state => {
+            data.isActive = state.isActive;
+            chrome.storage.sync.get("baseline", bet => {
+              data.baseline = bet.baseline || 1;
+              chrome.storage.sync.get("current", bet => {
+                data.current = parseInt(bet.current) || parseInt(data.baseline);
+                if (isFailure) {
+                  data.profit -= data.current;
+                  data.current = bet.current*2;
+                } else if (isSuccess) {
+                  data.profit += data.current;
+                  data.current = data.baseline;
+                } else {
+                  data.profit = 0;
+                  data.isActive = 0;
+                }
+                chrome.storage.sync.set({current: data.current});
+                chrome.storage.sync.set({profit: data.profit});
+                chrome.storage.sync.set({isActive: data.isActive});
 
-  chrome.storage.sync.get("profit", state => {
-    states.profit = state.profit || 0;
-    chrome.storage.sync.get("isActive", state => {
-      states.isActive = state.isActive;
-      chrome.storage.sync.get("baseline", bet => {
-        states.baseline = bet.baseline || 1;
-        chrome.storage.sync.get("current", bet => {
-          states.current = parseInt(bet.current) || parseInt(states.baseline);
-          if (isFailure) {
-            states.profit -= states.current;
-            states.current = bet.current*2;
-          } else if (isSuccess) {
-            states.profit += states.current;
-            states.current = states.baseline;
-          } else {
-            states.profit = 0;
-            states.isActive = 0;
-          }
-          chrome.storage.sync.set({current: states.current});
-          chrome.storage.sync.set({profit: states.profit});
-          chrome.storage.sync.set({isActive: states.isActive});
+                function timeout() {
+                  setTimeout(() => {
+                    input.val(data.current);
 
-          function timeout() {
-            setTimeout(() => {
-              input.val(states.current);
+                    setTimeout(() => {
+                      form.submit();
+                    },250)
+                  }, Math.floor(Math.random()*1000)+250)
+                }
 
-              setTimeout(() => {
-                form.submit();
-              },250)
-            }, Math.floor(Math.random()*1000)+250)
-          }
+                let header = $('.kt-subheader__title');
+                let newHeader = ` | Gambler: [ ${data.isActive} | ${data.baseline} | ${data.current} ]`;
+                header.append(newHeader);
 
-          let header = $('.kt-subheader__title');
-          let newHeader = ` | Gambler: [ ${states.isActive} | ${states.baseline} | ${states.current} ]`;
-          header.append(newHeader);
+                let profitDisplay = $('<span></span>');
+                if (data.profit > 0) {
+                  profitDisplay.text(`+${data.profit}`);
+                  profitDisplay.css({"color": "green"})
+                } else if (data.profit < 0) {
+                  profitDisplay.text(`${data.profit}`);
+                  profitDisplay.css({"color": "red"})
+                } else {
+                  profitDisplay.text(data.profit);
+                }
+                profitDisplay.css({"margin-left": '1rem'});
+                header.append(profitDisplay);
 
-          let profitDisplay = $('<span></span>');
-          if (states.profit > 0) {
-            profitDisplay.text(`+${states.profit}`);
-            profitDisplay.css({"color": "green"})
-          } else if (states.profit < 0) {
-            profitDisplay.text(`${states.profit}`);
-            profitDisplay.css({"color": "red"})
-          } else {
-            profitDisplay.text(states.profit);
-          }
-          profitDisplay.css({"margin-left": '1rem'});
-          header.append(profitDisplay);
+                let initButton = $('<button></button>')
+                  .text(data.isActive ? 'Stop' : 'Begin')
+                  .css({"margin-left": '1rem', "border-radius": "1rem"})
+                  .click(() => {
+                    if (data.isActive) {
+                      chrome.storage.sync.set({isActive: 0})
+                    } else {
+                      chrome.storage.sync.set({isActive: 1});
+                      timeout()
+                    }
+                  });
+                header.append(initButton);
 
-          let initButton = $('<button></button>')
-            .text(states.isActive ? 'Stop' : 'Begin')
-            .css({"margin-left": '1rem', "border-radius": "1rem"})
-            .click(() => {
-              if (states.isActive) {
-                chrome.storage.sync.set({isActive: 0})
-              } else {
-                chrome.storage.sync.set({isActive: 1});
-                timeout()
-              }
+                if (data.isActive) {
+                  timeout();
+                }
+              });
             });
-          header.append(initButton);
-
-          if (states.isActive) {
-            timeout();
-          }
+          });
         });
-      });
-    });
-  });
-}
+      }
+    }
+  },
+  travel: {
+    data: {
 
-function travel() {
+    },
+    step: () => {
+      let step   = document.getElementsByClassName('stepbuttonnew')[0];
+      let attack = document.getElementsByClassName('cta');
+      setInterval(() => {
+        if (attack[0].textContent === ' Attack') attack[0].click(); else
+        if (step.textContent === "Take another step") step.click()
+      }, 1000);
+    }
+  },
+  battle: {
+    data: {
 
+    },
+    attack: () => {
+
+    }
+  }
+};
+
+function figureOut() {
+  if (tab !== '/travel' && tab.indexOf('travel') !== -1) {
+    engine.travel.battle();
+  }
 }
 
 //
@@ -102,10 +132,11 @@ let tab = window.location.pathname;
 
 switch (tab) {
   case '/gamecentre/5050':
-    console.log('you mama');
-    gamble5050();
+    engine.gamble.oneInTwo.roll();
     break;
   case '/travel':
-    travel();
+    engine.travel.step();
     break;
+  default:
+    figureOut();
 }
