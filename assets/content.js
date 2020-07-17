8a5a13d45e818c21f98250c3073ee2b50ce6279b
Sugar_F0x <malion.torchwood@yandex.ru>
2020-07-17 16:04:10 +0300

    :corn: Suspend gamble module as it is in need of a rework
    This module used to work on chrome.storage, but since this permission
      is now deprecated for this plugin in favor of localStorage,
      gamble code portion is in need of a major rework

--- assets/content.js
+++ assets/content.js
@@ -75,102 +75,102 @@ let engine = {
       data[key] = engine[page].data[key].value;
     localStorage.setItem('SA_' + page, JSON.stringify(data));
   },
-  gamble: {
-    oneInTwo: {
-      data: {
-        isActive: null,
-        current:  null,
-        baseline: null,
-        profit:   null
-      },
-      roll:() => {
-        let data = engine.gamble.oneInTwo.data;
-        
-        let isSuccess = $('.notice-success').length > 0;
-        let isFailure = $('.notice-danger').length > 0;
-        let input     = $('#sample1');
-        let form      = $('#submit');
-
-        // TODO: add gold/min display
-        // TODO: add lose-streak display and fail-safe for when lose-streak goes too bad
-        // TODO: add longest win/lose streaks this runtime
-        // TODO: add input for baseline value inside DOM
-        // TODO: fix bug with storage overflow after long term use
-        // TODO: add documentation
-        // TODO: use async await? question mark?
-
-        chrome.storage.sync.get("profit", state => {
-          data.profit = state.profit || 0;
-          chrome.storage.sync.get("isActive", state => {
-            data.isActive = state.isActive;
-            chrome.storage.sync.get("baseline", bet => {
-              data.baseline = bet.baseline || 1;
-              chrome.storage.sync.get("current", bet => {
-                data.current = parseInt(bet.current) || parseInt(data.baseline);
-                if (isFailure) {
-                  data.profit -= data.current;
-                  data.current = bet.current*2;
-                } else if (isSuccess) {
-                  data.profit += data.current;
-                  data.current = data.baseline;
-                } else {
-                  data.profit = 0;
-                  data.isActive = 0;
-                }
-                chrome.storage.sync.set({current: data.current});
-                chrome.storage.sync.set({profit: data.profit});
-                chrome.storage.sync.set({isActive: data.isActive});
-
-                function timeout() {
-                  setTimeout(() => {
-                    input.val(data.current);
-
-                    setTimeout(() => {
-                      form.submit();
-                    },250)
-                  }, Math.floor(Math.random()*1000)+250)
-                }
-
-                let header = $('.kt-subheader__title');
-                let newHeader = ` | Gambler: [ ${data.isActive} | ${data.baseline} | ${data.current} ]`;
-                header.append(newHeader);
-
-                let profitDisplay = $('<span></span>');
-                if (data.profit > 0) {
-                  profitDisplay.text(`+${data.profit}`);
-                  profitDisplay.css({"color": "green"})
-                } else if (data.profit < 0) {
-                  profitDisplay.text(`${data.profit}`);
-                  profitDisplay.css({"color": "red"})
-                } else {
-                  profitDisplay.text(data.profit);
-                }
-                profitDisplay.css({"margin-left": '1rem'});
-                header.append(profitDisplay);
-
-                let initButton = $('<button></button>')
-                  .text(data.isActive ? 'Stop' : 'Begin')
-                  .css({"margin-left": '1rem', "border-radius": "1rem"})
-                  .click(() => {
-                    if (data.isActive) {
-                      chrome.storage.sync.set({isActive: 0})
-                    } else {
-                      chrome.storage.sync.set({isActive: 1});
-                      timeout()
-                    }
-                  });
-                header.append(initButton);
-
-                if (data.isActive) {
-                  timeout();
-                }
-              });
-            });
-          });
-        });
-      }
-    }
-  },
+  // gamble: {
+  //   oneInTwo: {
+  //     data: {
+  //       isActive: null,
+  //       current:  null,
+  //       baseline: null,
+  //       profit:   null
+  //     },
+  //     roll:() => {
+  //       let data = engine.gamble.oneInTwo.data;
+  //
+  //       let isSuccess = $('.notice-success').length > 0;
+  //       let isFailure = $('.notice-danger').length > 0;
+  //       let input     = $('#sample1');
+  //       let form      = $('#submit');
+  //
+  //       // TODO: add gold/min display
+  //       // TODO: add lose-streak display and fail-safe for when lose-streak goes too bad
+  //       // TODO: add longest win/lose streaks this runtime
+  //       // TODO: add input for baseline value inside DOM
+  //       // TODO: fix bug with storage overflow after long term use
+  //       // TODO: add documentation
+  //       // TODO: use async await? question mark?
+  //
+  //       chrome.storage.sync.get("profit", state => {
+  //         data.profit = state.profit || 0;
+  //         chrome.storage.sync.get("isActive", state => {
+  //           data.isActive = state.isActive;
+  //           chrome.storage.sync.get("baseline", bet => {
+  //             data.baseline = bet.baseline || 1;
+  //             chrome.storage.sync.get("current", bet => {
+  //               data.current = parseInt(bet.current) || parseInt(data.baseline);
+  //               if (isFailure) {
+  //                 data.profit -= data.current;
+  //                 data.current = bet.current*2;
+  //               } else if (isSuccess) {
+  //                 data.profit += data.current;
+  //                 data.current = data.baseline;
+  //               } else {
+  //                 data.profit = 0;
+  //                 data.isActive = 0;
+  //               }
+  //               chrome.storage.sync.set({current: data.current});
+  //               chrome.storage.sync.set({profit: data.profit});
+  //               chrome.storage.sync.set({isActive: data.isActive});
+  //
+  //               function timeout() {
+  //                 setTimeout(() => {
+  //                   input.val(data.current);
+  //
+  //                   setTimeout(() => {
+  //                     form.submit();
+  //                   },250)
+  //                 }, Math.floor(Math.random()*1000)+250)
+  //               }
+  //
+  //               let header = $('.kt-subheader__title');
+  //               let newHeader = ` | Gambler: [ ${data.isActive} | ${data.baseline} | ${data.current} ]`;
+  //               header.append(newHeader);
+  //
+  //               let profitDisplay = $('<span></span>');
+  //               if (data.profit > 0) {
+  //                 profitDisplay.text(`+${data.profit}`);
+  //                 profitDisplay.css({"color": "green"})
+  //               } else if (data.profit < 0) {
+  //                 profitDisplay.text(`${data.profit}`);
+  //                 profitDisplay.css({"color": "red"})
+  //               } else {
+  //                 profitDisplay.text(data.profit);
+  //               }
+  //               profitDisplay.css({"margin-left": '1rem'});
+  //               header.append(profitDisplay);
+  //
+  //               let initButton = $('<button></button>')
+  //                 .text(data.isActive ? 'Stop' : 'Begin')
+  //                 .css({"margin-left": '1rem', "border-radius": "1rem"})
+  //                 .click(() => {
+  //                   if (data.isActive) {
+  //                     chrome.storage.sync.set({isActive: 0})
+  //                   } else {
+  //                     chrome.storage.sync.set({isActive: 1});
+  //                     timeout()
+  //                   }
+  //                 });
+  //               header.append(initButton);
+  //
+  //               if (data.isActive) {
+  //                 timeout();
+  //               }
+  //             });
+  //           });
+  //         });
+  //       });
+  //     }
+  //   }
+  // },
   travel: {
     data: {
       isAuto: {
