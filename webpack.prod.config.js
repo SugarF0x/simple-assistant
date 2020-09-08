cf36bdafd241c55226ab7ae111d901baa1abe619
SugarF0x <malion.torchwood@yandex.ru>
2020-09-08 17:58:20 +0300

    :white_check_mark: Automatically pull package version to manifest
    > Automatically pull version on compilation from package.json
    > Automatically set version_name based on compilation type
      * Dev build will have it set to IN-DEV
      * Production will have it set to STABLE

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,38 +1,42 @@
-// const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const VERSION = require('./package.json').version;
 
 module.exports = {
-    watchOptions: {
-        aggregateTimeout: 300,
-        ignored: [
-            /node_modules/
-        ]
-    },
-    entry: path.resolve(__dirname, 'src/assets/index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "js/[name].bundle.js"
-    },
-    target: "web",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    // plugins: [
-    //     new CopyPlugin([
-    //         {
-    //             from: 'src/img',
-    //             to: 'img/[path][name].[ext]',
-    //             toType: 'template'
-    //         }
-    //     ])
-    // ],
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
-    },
+  entry: path.resolve(__dirname, 'src/assets/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js"
+  },
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/static',
+          transform(content, path) {
+            if (path.indexOf('manifest') !== -1) {
+              let manifest = JSON.parse(content.toString());
+              manifest.version = VERSION;
+              manifest.version_name = 'STABLE';
+              return Buffer.from(JSON.stringify(manifest));
+            }
+            return content;
+          },
+        }
+      ],
+    })
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
 };
