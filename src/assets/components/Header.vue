42c332c23caaf2225b0df4afd902a53f01237aa5
SugarF0x <malion.torchwood@yandex.ru>
2020-09-17 10:52:46 +0300

    :white_check_mark: Add Header component
    Header component directs rendering of Title containing:
      * Extension name
      * Extension version
      * Currently active module

---
+++ src/assets/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>
+      <span>
+        Simple Assistant v{{ version }}
+      </span>
+      <span class="module">
+        {{ module }}
+      </span>
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Header",
+  data() {
+    return {
+      version: chrome.runtime.getManifest().version_name
+    }
+  },
+  props: [
+      'module'
+  ]
+}
+</script>
+
+<style lang="less" scoped>
+  h2 {
+    display: flex;
+    justify-content: space-between;
+  }
+  .module {
+    opacity: .5;
+    text-transform: uppercase;
+  }
+</style>
