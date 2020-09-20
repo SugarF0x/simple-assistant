859f31b28099d37e7671f30a255f3d5e92ed256c
Sugar_F0x <malion.torchwood@yandex.ru>
2020-09-21 00:05:50 +0300

    :white_check_mark: Add Arena module functionality
    > Arena module works just as it did with two caveats:
      * jQuery is no more - rewritten in native js
      * Cycle functionality is suspended until further notice

--- src/assets/modules/Arena.vue
+++ src/assets/modules/Arena.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="checkbox">
-      <label><input type="checkbox" v-model="isAuto"></label>
+      <label><input type="checkbox" v-model="options.isAuto"></label>
       <span>Auto-accept enemy generation prompts</span>
     </div>
     <button class="btn btn-primary" @click="fightAll">Auto fight</button>
@@ -9,23 +9,64 @@
 </template>
 
 <script lang="ts">
+import module from '../mixins/module';
+
 export default {
   name: "Arena",
+  mixins: [module],
 
   data() {
     return {
-      isAuto: false
+      version: 1,
+      options: {
+        isAuto: false,
+        fightAll: false
+      }
     }
   },
 
   methods: {
     fightAll() {
-      // spend all power on fighting at arena
-
-      //          Legacy code
-      // localStorage.setItem('SA_arena_tmp', JSON.stringify({fightAll:true}));
-      // $('.btn-custom')[0].click();
+      this.options.fightAll = true;
+      let generate = document.getElementsByClassName('btn-custom')[0] as HTMLButtonElement;
+      generate.click();
     }
+  },
+
+  mounted() {
+    let generate = document.getElementsByClassName('btn-custom')[0] as HTMLButtonElement;
+
+    if (this.options.fightAll)
+      if (parseInt(document.getElementById('current_energy').textContent) > 0)
+        setTimeout(() => {generate.click()}, 250);
+      else
+        this.options.fightAll = false;
+
+    generate.addEventListener('click', () => {
+      if (this.options.isAuto || this.options.fightAll) {
+        let interval = setInterval(() => {
+          try {
+            let button = document.getElementsByClassName('swal2-confirm')[0] as HTMLButtonElement;
+            if (button.textContent.indexOf('generate') !== -1 || button.textContent.indexOf('Attack') !== -1 )
+              button.click()
+          } catch (err) {
+            clearInterval(interval)
+          }
+        }, 1000 + Math.floor(Math.random() * 500))
+      }
+    });
+
+    //          Legacy Cycle
+    // if (engine.home.data.state.value === 'standby' && engine.home.data.stage.value === 2) {
+    //   if (parseInt($('#current_energy').text()) > 0)
+    //     setTimeout(() => {
+    //       generate.click();
+    //     }, 1000);
+    //   else {
+    //     engine.home.data.state.value = 'pending';
+    //     engine.$set('home');
+    //   }
+    // }
   }
 }
 </script>
