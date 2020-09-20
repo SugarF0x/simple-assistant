e7b8fcba3d0cd6d3a7622a29c8be331b38fba7fc
Sugar_F0x <malion.torchwood@yandex.ru>
2020-09-20 23:49:04 +0300

    :white_check_mark: Add Battle module functionality
    > Battle module works just as it did apart from whats under the hood:
      * jQuery is no more - rewritten in native js

--- src/assets/modules/Battle.vue
+++ src/assets/modules/Battle.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
     <div class="checkbox">
-      <label><input type="checkbox" v-model="isAuto"></label>
+      <label><input type="checkbox" v-model="options.isAuto"></label>
       <span>Attack enemy on cooldown</span>
     </div>
     <div class="checkbox">
-      <label><input type="checkbox" v-model="goBack"></label>
+      <label><input type="checkbox" v-model="options.goBack"></label>
       <span>Go back when enemy is defeated</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import module from '../mixins/module';
+
 export default {
   name: "Battle",
+  mixins: [module],
 
   data() {
     return {
-      isAuto: false,
-      goBack: false
+      version: 1,
+      options: {
+        isAuto: false,
+        goBack: false
+      }
     }
+  },
+
+  mounted() {
+    let attack = document.getElementById('attackButton');
+    let back   = document.getElementsByClassName('btn-info')[0] as HTMLButtonElement;
+    let enemy  = document.getElementById('enemyBox');
+
+    let interval = setInterval(() => {
+      try {
+        if (this.options.isAuto) {
+          if (enemy.style.opacity === '0.1') {
+            clearInterval(interval);
+            if (this.options.goBack) setTimeout(() => {back.click()}, 500);
+          } else if (attack.innerText === 'Attack') attack.click();
+        }
+      } catch (err) {
+        clearInterval(interval);
+      }
+    },1200+Math.floor(Math.random()*400))
   }
 }
 </script>
