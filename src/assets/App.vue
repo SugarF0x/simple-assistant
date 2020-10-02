dea0ed37a0aa9a6099b517b0d275a336efeb7b62
Sugar_F0x <malion.torchwood@yandex.ru>
2020-10-02 11:21:32 +0300

    :white_check_mark: Add SAD collapse button
    Press it to collapse/expand SAD in case its getting too big

--- src/assets/App.vue
+++ src/assets/App.vue
@@ -3,17 +3,26 @@
     <div class="row">
       <div class="col sad">
         <Header :module="module"/>
-        <router-view></router-view>
+        <div :style="{height: options.collapse ? 0 : 'inherit'}" class="router-wrapper">
+          <router-view/>
+        </div>
       </div>
+    </div>
+    <div @click="options.collapse = !options.collapse"
+         class="collapseButton"
+    >
+      <div style="display: inline-block" :style="{transform: options.collapse ? 'rotate(270deg)' : 'rotate(90deg)'}"><</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Header from './components/Header.vue';
+import module from './mixins/module';
 
 export default {
   name: "App",
+  mixins: [module],
 
   components: {
     Header
@@ -21,7 +30,10 @@ export default {
 
   data() {
     return {
-
+      version: 1,
+      options: {
+        collapse: false
+      }
     }
   },
 
@@ -43,13 +55,26 @@ export default {
 
 <style lang="less" scoped>
   .col {
-    padding: 1rem;
+    padding: 1rem 1rem 0 1rem;
+  }
+  .router-wrapper {
+    transition: height .5s;
+    overflow: hidden;
+  }
+  .collapseButton {
+    cursor: pointer;
+    text-align: center;
+    font-size: 2em;
+    font-weight: bold;
+  }
+  .collapseButton:hover {
+    background-color: gainsboro;
   }
 </style>
 
 <style lang="less">
   .sad {
-    > * > * {
+    > * > * > * {
       margin: .3rem 0;
     }
     label {
