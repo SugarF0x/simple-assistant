7c8ff11f909d19a433c71f0790d64ab914e2aaf4
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-27 19:12:25 +0300

    dev: add manifest with transform

--- vue.config.js
+++ vue.config.js
@@ -8,14 +8,35 @@ const version = process.env.NODE_ENV === "production" ? VERSION : `${VERSION}.${
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  filenameHashing: false,
+  css: {
+    extract: {
+      filename: "styles.css",
+    },
+  },
   chainWebpack: (config) => {
     // TODO:
-    //  * add manifest
     //  * add hot extension reload
 
     config.plugins.delete("html")
     config.plugins.delete("preload")
     config.plugins.delete("prefetch")
+
+    config.plugin("copy").tap((args) => {
+      args[0].patterns.push({
+        from: "extension/manifest.json",
+        transform(content) {
+          let manifest = JSON.parse(content.toString())
+          manifest.version = version
+          manifest.version_name = version
+          // manifest.background = { scripts: ["lib/hot-reload.js"] };
+
+          return Buffer.from(JSON.stringify(manifest, null, 2))
+        },
+      })
+
+      return args
+    })
 
     return config
   },
