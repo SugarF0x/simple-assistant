399d9fd19c3034561aaa5d27b9afb23d1df293ea
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-27 19:19:23 +0300

    dev: add hot-reload

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("@vue/cli-service")
 const VERSION = require("./package.json").version
 const REVISION = require("child_process").execSync("git rev-list --count HEAD").toString()
 
-const version = process.env.NODE_ENV === "production" ? VERSION : `${VERSION}.${REVISION}`
+const isDev = process.env.NODE_ENV !== "production"
+
+const version = isDev ? `${VERSION}.${REVISION}` : VERSION
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -15,9 +17,6 @@ module.exports = defineConfig({
     },
   },
   chainWebpack: (config) => {
-    // TODO:
-    //  * add hot extension reload
-
     config.plugins.delete("html")
     config.plugins.delete("preload")
     config.plugins.delete("prefetch")
@@ -29,11 +28,14 @@ module.exports = defineConfig({
           let manifest = JSON.parse(content.toString())
           manifest.version = version
           manifest.version_name = version
-          // manifest.background = { scripts: ["lib/hot-reload.js"] };
+
+          if (isDev) manifest.background = { scripts: ["hot-reload.js"] };
 
           return Buffer.from(JSON.stringify(manifest, null, 2))
         },
       })
+
+      if (isDev) args[0].patterns.push({ from: "extension/hot-reload.js" })
 
       return args
     })
