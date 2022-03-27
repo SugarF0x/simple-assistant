73a3a913d39c17de58ea53188bbe6f629a085dcb
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-27 18:11:40 +0300

    disable html webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 const { defineConfig } = require("@vue/cli-service")
 
+const VERSION = require("./package.json").version
+const REVISION = require("child_process").execSync("git rev-list --count HEAD").toString()
+
+const version = process.env.NODE_ENV === "production" ? VERSION : `${VERSION}.${REVISION}`
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  chainWebpack: (config) => {
+    // TODO:
+    //  * add manifest
+    //  * add hot extension reload
+
+    config.plugins.delete("html")
+    config.plugins.delete("preload")
+    config.plugins.delete("prefetch")
+
+    return config
+  },
 })
