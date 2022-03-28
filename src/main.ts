10e77a8c2254bd68157a006de450de643a72b5d7
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-28 20:58:32 +0300

    dev: add footer app mount

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
-createApp(App).use(router).mount("#app");
+const app = document.createElement("div")
+app.id = "app"
+document.querySelector("footer")?.after(app)
+
+createApp(App).use(router).mount("#app")
