5f760040711dfc49eeb1fbccf0a692d9a1da9d25
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-04-02 12:07:44 +0300

    refactor(travel): transform useTravelButton to TravelButton component

--- src/views/Travel/Travel.vue
+++ src/views/Travel/Travel.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import TravelBridge from "./TravelBridge.vue"
 import TravelBar from "./TravelBar.vue"
-
-import { useTravelButton } from "./useTravelButton"
-
-useTravelButton()
+import TravelButton from "./TravelButton.vue"
 </script>
 
 <template>
   <TravelBridge />
   <TravelBar />
+  <TravelButton />
 </template>
---
+++ src/views/Travel/TravelButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useTravelStore } from "@/store"
+import { storeToRefs } from "pinia"
+import { onMounted } from "vue"
+
+const travelStore = useTravelStore()
+const { cooldownTimeLeft } = storeToRefs(travelStore)
+
+const travelButton = document.querySelector<HTMLButtonElement>(".px-4.py-4 button")
+
+onMounted(() => {
+  if (!travelButton) return
+  if (!cooldownTimeLeft.value) return
+  travelButton.disabled = true
+
+  setTimeout(() => {
+    travelButton.disabled = false
+  }, cooldownTimeLeft.value)
+})
+</script>
+
+<template>
+  <slot />
+</template>
