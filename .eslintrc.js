58085ca51684608466effe6d1d9b365a212fe7fa
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-04-03 14:38:22 +0300

    style(lint): add chrome as global lint variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  globals: {
+    chrome: true,
+  },
 }
