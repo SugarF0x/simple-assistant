6d94fadaf6a8894003789aaa5a1117bd25daff62
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-04-17 16:07:25 +0300

    fix(navigation): add default shortcuts

--- src/views/Navigation/store.ts
+++ src/views/Navigation/store.ts
@@ -7,7 +7,13 @@ export const useNavigationStore = defineStore(
     const shouldUseShortcuts = ref(false)
     const shouldEditShortcuts = ref(false)
 
-    const urlToKeyMap = ref<Record<string, string>>({})
+    const urlToKeyMap = ref<Record<string, string>>({
+      "/home": "Digit1",
+      "/battle/menu": "Digit2",
+      "/quests/viewall": "Digit3",
+      "/jobs/viewall": "Digit4",
+      "/tasks/viewall": "Digit5",
+    })
     const keyToUrlMap = computed<Record<string, string>>(() =>
       Object.fromEntries(Object.entries(urlToKeyMap.value).map((e) => e.reverse()))
     )
