176d458454932f78f6d1f8a097a63630c1a06134
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-11-09 18:45:52 +0300

    randomize leaf size & rotation

--- src/views/Travel/Leaves/Leaf.vue
+++ src/views/Travel/Leaves/Leaf.vue
@@ -7,8 +7,11 @@ import leaf4 from "@/assets/svgs/leaf-4.svg"
 const leaves = [leaf1, leaf2, leaf3, leaf4].map((e) => chrome.runtime.getURL(e))
 
 const index = Math.floor(Math.random() * leaves.length)
+
 const startX = Math.floor(Math.random() * 100) + "%"
 const startY = Math.floor(Math.random() * 100) + "%"
+const startSize = Math.floor(Math.random() * 5) / 10 + 0.5
+const startRotation = Math.floor(Math.random() * 360) + "deg"
 </script>
 
 <template>
@@ -20,7 +23,8 @@ const startY = Math.floor(Math.random() * 100) + "%"
   position: absolute;
   top: v-bind(startX);
   left: v-bind(startY);
-  width: 24px;
-  height: 24px;
+  transform: scale(v-bind(startSize)) rotate(v-bind(startRotation));
+  width: 32px;
+  height: 32px;
 }
 </style>
