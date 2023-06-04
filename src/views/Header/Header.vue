a82eeb6e0d4eb8f61913cc99dcbc9c99da0576ed
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-06-04 11:36:54 +0300

    bump supported version

--- src/views/Header/Header.vue
+++ src/views/Header/Header.vue
@@ -9,7 +9,7 @@ const ftueStore = useFtueStore()
 const { isDismissed } = storeToRefs(ftueStore)
 
 const version = chrome.runtime.getManifest().version
-const gameVersion = "12.17"
+const gameVersion = "12.25.4"
 </script>
 
 <template>
